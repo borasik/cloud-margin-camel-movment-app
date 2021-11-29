9fc02005ed036eaa20e847ee6eeb2017605c2dfd
Alex Borsuk <borasik@hotmail.com>
2021-11-29 18:32:03 -0500

    checkpint - webhook approval done

---
+++ src/main/java/org/ffdc/data/platform/Models/AzureBlobSubscriptionValidationResponseBody.java
@@ -0,0 +1,42 @@
+package org.ffdc.data.platform.Models;
+
+import java.util.HashMap;
+import java.util.Map;
+import javax.annotation.Generated;
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({ "validationResponse" })
+@Generated("jsonschema2pojo")
+
+public class AzureBlobSubscriptionValidationResponseBody {
+    @JsonProperty("validationResponse")
+    private String validationResponse;
+    @JsonIgnore
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+    @JsonProperty("validationResponse")
+    public String getValidationResponse() {
+        return validationResponse;
+    }
+
+    @JsonProperty("validationResponse")
+    public void setValidationResponse(String validationResponse) {
+        this.validationResponse = validationResponse;
+    }
+
+    @JsonAnyGetter
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    @JsonAnySetter
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+}
